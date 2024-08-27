bb35a3e01a710293c1919c597097c132d268a2e7
Your Name <you@example.com>
2024-08-27 01:49:53 +0000

    TRIANGLE

--- src/main.c
+++ src/main.c
@@ -20,6 +20,12 @@ int main()
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
         glfwSwapBuffers(window);
 
         glfwPollEvents();
